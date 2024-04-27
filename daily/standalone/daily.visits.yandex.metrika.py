ef1a8f0e3a1e662acfaa260257484a314a94fd25
Nikolay Matsievsky <speed@webo.name>
2024-04-27 11:48:24 +0300

    Fixed date condition in Yandex Metrika daily

--- daily/standalone/daily.visits.yandex.metrika.py
+++ daily/standalone/daily.visits.yandex.metrika.py
@@ -34,12 +34,13 @@ if config["DB"]["TYPE"] in ["MYSQL", "POSTGRESQL", "MARIADB", "ORACLE", "SQLITE"
         connection.execute(text('SET character_set_connection=utf8mb4'))
 
 api = YandexMetrikaLogsapi(access_token=config["YANDEX_METRIKA"]["ACCESS_TOKEN"], default_url_params={'counterId': config["YANDEX_METRIKA"]["COUNTER_ID"]})
-# Создание запроса на выгрузку данных (вчера)
+# Создание запроса на выгрузку данных (вчера + позавчера)
 yesterday = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+yesterday_1 = (date.today() - timedelta(days=2)).strftime('%Y-%m-%d')
 params = {
     "fields": "ym:s:visitID,ym:s:dateTime,ym:s:isNewUser,ym:s:startURL,ym:s:endURL,ym:s:pageViews,ym:s:visitDuration,ym:s:ipAddress,ym:s:regionCountry,ym:s:regionCity,ym:s:regionCountryID,ym:s:regionCityID,ym:s:clientID,ym:s:networkType,ym:s:goalsID,ym:s:referer,ym:s:from,ym:s:lastTrafficSource,ym:s:lastAdvEngine,ym:s:lastReferalSource,ym:s:lastSearchEngineRoot,ym:s:lastSearchEngine,ym:s:lastSocialNetwork,ym:s:lastSocialNetworkProfile,ym:s:lastDirectClickOrder,ym:s:lastDirectPlatformType,ym:s:lastDirectPlatform,ym:s:lastUTMCampaign,ym:s:lastUTMContent,ym:s:lastUTMMedium,ym:s:lastUTMSource,ym:s:lastUTMTerm,ym:s:lastRecommendationSystem,ym:s:lastGCLID,ym:s:lastMessenger,ym:s:browser,ym:s:browserLanguage,ym:s:browserCountry,ym:s:deviceCategory,ym:s:mobilePhone,ym:s:mobilePhoneModel,ym:s:operatingSystemRoot,ym:s:operatingSystem,ym:s:browserMajorVersion,ym:s:browserMinorVersion,ym:s:browserEngine,ym:s:browserEngineVersion1,ym:s:browserEngineVersion2,ym:s:browserEngineVersion3,ym:s:browserEngineVersion4",
     "source": "visits",
-    "date1": yesterday,
+    "date1": yesterday_1,
     "date2": yesterday
 }
 # удаляем все текущие запросы API
@@ -82,13 +83,13 @@ for p in info["log_request"]["parts"]:
 # добавляем метку времени
         data["ts"] = pd.DatetimeIndex(data["ym:s:dateTime"]).asi8
         if config["DB"]["TYPE"] in ["MYSQL", "POSTGRESQL", "MARIADB", "ORACLE", "SQLITE"]:
-            connection.execute(text('DELETE FROM ' + config["YANDEX_METRIKA"]["TABLE_VISITS"] + ' WHERE `ym:s:dateTime`>=' + yesterday))
+            connection.execute(text("DELETE FROM " + config["YANDEX_METRIKA"]["TABLE_VISITS"] + " WHERE `ym:s:dateTime`>='" + yesterday_1 + "'"))
             connection.commit()
             data.to_sql(name=config["YANDEX_METRIKA"]["TABLE_VISITS"], con=engine, if_exists='append', chunksize=100)
         elif config["DB"]["TYPE"] == "CLICKHOUSE":
 # удаляем данные за вчера
             requests.post('https://' + config["DB"]["USER"] + ':' + config["DB"]["PASSWORD"] + '@' + config["DB"]["HOST"] + ':8443/',
-                params={"database": config["DB"]["DB"], "query": 'DELETE FROM ' + config["DB"]["DB"] + '.' + config["YANDEX_METRIKA"]["TABLE_VISITS"] + ' WHERE `ym:s:dateTime`>=' + yesterday}, headers={'Content-Type':'application/octet-stream'}, verify=False)
+                params={"database": config["DB"]["DB"], "query": "DELETE FROM " + config["DB"]["DB"] + "." + config["YANDEX_METRIKA"]["TABLE_VISITS"] + " WHERE `ym:s:dateTime`>='" + yesterday_1 + "'"}, headers={'Content-Type':'application/octet-stream'}, verify=False)
 # добавляем новые данные
             csv_file = data.to_csv().encode('utf-8')
             requests.post('https://' + config["DB"]["USER"] + ':' + config["DB"]["PASSWORD"] + '@' + config["DB"]["HOST"] + ':8443/',
