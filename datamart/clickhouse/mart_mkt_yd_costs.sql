27cbc57a02a88695ffbce528eb23bdf46480d2ac
Nikolay Matsievsky <speed@webo.name>
2024-05-15 00:47:51 +0300

    Fixed Yandex Direct costs mart for Clickhouse

--- datamart/clickhouse/mart_mkt_yd_costs.sql
+++ datamart/clickhouse/mart_mkt_yd_costs.sql
@@ -1,16 +1,16 @@
 CREATE VIEW DB.mart_mkt_yd_costs AS SELECT
-    SUM(IFNULL(`c.Cost`,0)) AS COSTS,
-    toDate(`c.Date`) AS DT,
+    SUM(IFNULL(`Cost`,0)) AS COSTS,
+    toDate(`Date`) AS DT,
     CASE
-        WHEN `c.AdNetworkType`='AD_NETWORK' THEN IFNULL(u.UTMSource, 'yandex_network')
-        WHEN `c.AdNetworkType`='SEARCH' THEN IFNULL(u.UTMSource, 'yandex')
+        WHEN `AdNetworkType`='AD_NETWORK' THEN IFNULL(u.UTMSource, 'yandex_network')
+        WHEN `AdNetworkType`='SEARCH' THEN IFNULL(u.UTMSource, 'yandex')
     END AS UTM_SOURCE_PURE,
-    toString(`c.CampaignId`) AS UTM_CAMPAIGN_ID,
+    toString(c.CampaignId) AS UTM_CAMPAIGN_ID,
     IFNULL(u.UTMMedium, 'cpc') AS UTM_MEDIUM_PURE,
-	IFNULL(u.UTMCampaign, c.CampaignId) AS UTM_CAMPAIGN_PURE,
-    `c.CampaignName` AS CAMPAIGN_NAME
+	IFNULL(u.UTMCampaign, toString(c.CampaignId)) AS UTM_CAMPAIGN_PURE,
+    `CampaignName` AS CAMPAIGN_NAME
 FROM DB.raw_yd_costs as c
-	LEFT JOIN DB.raw_yd_campaigns_utms as u WHERE c.CampaignId=u.CampaignId
+	LEFT JOIN DB.raw_yd_campaigns_utms as u ON c.CampaignId=u.CampaignId
 GROUP BY UTM_CAMPAIGN_ID,CAMPAIGN_NAME,UTM_CAMPAIGN_PURE,UTM_MEDIUM_PURE,UTM_SOURCE_PURE,DT
 
 SETTINGS join_use_nulls = 1
