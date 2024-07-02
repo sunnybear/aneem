d70e76eb308944f36a24f9c6275c32bdd36d6317
Nikolay Matsievsky <speed@webo.name>
2024-07-02 21:13:07 +0300

    Improved dict phone<->lead/deal Bitrix24

--- datamart/clickhouse/dict_bxdealid_phone.sql
+++ datamart/clickhouse/dict_bxdealid_phone.sql
@@ -11,13 +11,13 @@ ORDER BY (ID, phone);
 CREATE MATERIALIZED VIEW DB.dict_bxdealid_phone_mv TO DB.dict_bxdealid_phone AS
 SELECT
     d.ID,
-    CONCAT('7', SUBSTRING(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), 2, 12)) AS phone
+    CONCAT('7', SUBSTRING(replace(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), '-', ''), 2, 12)) AS phone
 FROM DB.raw_bx_crm_deal as d
     LEFT JOIN DB.raw_bx_crm_contact_uf as c ON d.CONTACT_ID=c.ID;
 
 -- 3. initial data upload
 INSERT INTO DB.dict_bxdealid_phone SELECT
     d.ID,
-    CONCAT('7', SUBSTRING(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), 2, 12)) AS phone
+    CONCAT('7', SUBSTRING(replace(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), '-', ''), 2, 12)) AS phone
 FROM DB.raw_bx_crm_deal as d
     LEFT JOIN DB.raw_bx_crm_contact_uf as c ON d.CONTACT_ID=c.ID;
--- datamart/clickhouse/dict_bxleadlid_phone.sql
+++ datamart/clickhouse/dict_bxleadlid_phone.sql
@@ -11,11 +11,11 @@ ORDER BY (ID, phone);
 CREATE MATERIALIZED VIEW DB.dict_bxleadid_phone_mv TO DB.dict_bxleadid_phone AS
 SELECT
     ID,
-    CONCAT('7', SUBSTRING(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), 2, 12)) AS phone
+    CONCAT('7', SUBSTRING(replace(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), '-', ''), 2, 12)) AS phone
 FROM DB.raw_bx_crm_lead_uf;
 
 -- 3. initial data upload
 INSERT INTO DB.dict_bxleadid_phone SELECT
     ID,
-    CONCAT('7', SUBSTRING(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), 2, 12)) AS phone
+    CONCAT('7', SUBSTRING(replace(replace(replace(replace(replace(phone1, '(', ''), ')', ''), ' ', ''), '+', ''), '-', ''), 2, 12)) AS phone
 FROM DB.raw_bx_crm_lead_uf;
